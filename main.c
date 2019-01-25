0ab034128b4f44045b3c262728b83a368c7cb9f3
Troy Jones <sntnmjones@gmail.com>
2019-01-25 13:02:58 -0800

    Created dynamic arrays that are populated with rand().

--- main.c
+++ main.c
@@ -6,14 +6,18 @@
  *          do so.
  *
  *  Requirements:
- *      - use c99
+ *      + use c99
  *      - add 2 matrices that are 10,000 x 10,000
- *      - cmd arg : 'n' which will be the number of rows
- *      - use scanf to determine whether the array will be dynamic or automatic
+ *      + cmd arg : 'n' which will be the number of rows
+ *      + use scanf to determine whether the array will be dynamic or automatic
  *      - randomly populate arrays A & B
  *      - printf timing results of sum
+ *
+ *  References:
+ *      - https://www.geeksforgeeks.org/dynamically-allocate-2d-array-c/
 **/
 
+#include "time.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdbool.h"
@@ -21,7 +25,6 @@
 /*******************************************************************************
  *  FUNCTIONS
  ******************************************************************************/
-
 /**
  *  @brief: Prompts user to see what memory type to use to allocate array
  *           memory.
@@ -42,21 +45,64 @@ bool is_dynamic()
     return dynamic;
 }
 
+
+/**
+ *
+ */
+int* create_array(int grid_size)
+{
+    int *array = malloc(grid_size * grid_size * sizeof(int));
+    srand(time(NULL));
+
+    for (int row = 0; row < grid_size; row++)
+    {
+        for (int col = 0; col < grid_size; col++)
+        {
+            *(array + row * grid_size + col) = rand() % 100;
+        }
+    }
+
+    return array;
+}
+
+
+/**
+ *
+ */
+void dynamic_stuff(int grid_size)
+{
+    int *array_A = create_array(grid_size);
+    int *array_B = create_array(grid_size);
+
+
+    free(array_A);
+    free(array_B);
+}
+
+
+/**
+ *
+ */
+void auto_stuff(int grid_size)
+{
+    printf("Auto stuff not complete.\n");
+}
+
+
 /*******************************************************************************
  *  MAIN
  ******************************************************************************/
-
 int main(int argc, char const *argv[])
 {
-    int num_rows = atoi(argv[1]);
+    int grid_size = atoi(argv[1]);
 
     if (is_dynamic())
     {
-        printf("dynamic\n");
+        dynamic_stuff(grid_size);
     }
     else
     {
-        printf("auto\n");
+        auto_stuff(grid_size);
     }
 
     return 0;
