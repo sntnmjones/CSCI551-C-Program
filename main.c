96287913766f275e5155e85547394310576422e6
Troy Jones <sntnmjones@gmail.com>
2019-01-24 16:04:22 -0800

    Boilerplate

---
+++ main.c
@@ -0,0 +1,22 @@
+/**
+ *  Troy Jones
+ *
+ *  Purpose:
+ *      - Add two random matrices together and print out the time it takes to
+ *          do so.
+ *
+ *  Requirements:
+ *      - use c99
+ *      - add 2 matrices that are 10,000 x 10,000
+ *      - cmd arg : 'n' which will be the number of rows
+ *      - use scanf to determine whether the array will be dynamic or automatic
+ *      - randomly populate arrays A & B
+ *      - printf timing results of sum
+**/
+
+int main(int argc, char const *argv[])
+{
+
+
+    return 0;
+}
